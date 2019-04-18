02cfd05023b6a0f88e2703ebfcc4f2aac2792d9b
Chelsea Yue <cyue@lcs.on.ca>
2019-04-18 16:26:48 -0400

    finished adding the black keys but failed to put them in the right position

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -10,25 +10,26 @@ public class Key extends Actor
     //tracks whether is "piano key down" image has already been set or not
     // (isDown is a primitive data type - shows in red, type is lowercase)
     private boolean isDown;
-    
-    
+
     //Track what key is being played AND what sound file to play
     //key and sound areobject data types - shows in black, type i Capitalized)
     private String key;
     private String sound; 
-    
+    private boolean keyWhite;
+
     /**
      * Create a new key.
      * Constructor - it runs once when the Key object is created
      */
-    public Key(String keyName, String soundFile)
+    public Key(String keyName, String soundFile, boolean isKeyWhite)
     {
         // Key begins in the "up" position
         isDown = false;
-        
+
         //initialize the key ad sound instance variables
         key = keyName;
         sound = soundFile;
+        keyWhite = isKeyWhite;
         
     }
 
@@ -40,16 +41,25 @@ public class Key extends Actor
         //When the key image is NOT already showing "down"
         //AND
         //the "g" key on the keyboard has been pressed
-        if (! isDown && Greenfoot.isKeyDown(key))
+
+        // {(! isDown && Greenfoot.isKeyDown(key))
+        if (keyWhite == true)
         {
             setImage("white-key-down.png");
             isDown = true;
             play();
-        }
 
-        if ( isDown == true && Greenfoot.isKeyDown(key) == false)
-        {
+
             setImage("white-key.png");
+            isDown= false;
+        }
+        else
+        {
+            setImage("black-key-down.png");
+            isDown = true;
+            play();
+
+            setImage("black-key.png");
             isDown= false;
         }
     }
--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -18,11 +18,12 @@ public class Piano extends World
     //Data type:String, we want an array, and the name is "studentNames"
     String[] studentNames = {"Sarah", "Chelsea", "Jack", "Maxwell", "Gavin", "Minah", "Brandon",
             "Jeewoo", "Peter", "Justy"};
-    
+
     // Track the white keys and notes
     String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\"};
     String[] whiteNotes = {"3c", "3d","3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g"};
-
+    String[] blackKeys = {"w", "e", "", "t", "y", "u", "", "o", "p", "", "]", ""};
+    String[] blackNotes = {"3c#", "3d#","", "3f#", "3g#", "3a#", "", "4c#", "4d#", "", "4f#", ""};
     public Piano() 
     {
         super(800, 340, 1);
@@ -39,24 +40,33 @@ public class Piano extends World
     {
         //This block of code runs once a second until the end of the 
         //-whiteKey array
-        
+
         // && is a Boolean AND
         //statement in the conditional run only when both conditions are true
         int position = frames / 60;
         //NOTE: whiteKey/length automatically returns the coreect number
         //-of values in the array (so that we won't run past the end)
-        if ((frames % 60 == 0) && (position < whiteKeys.length))
+        if ((frames % 60 == 0) && (position < 24 ))//blackKeys.length))
         {
             //Assemble the piano by creating each key one by one
-            Key anotherKey = new Key(whiteKeys[position], whiteNotes[position]);
+            Key anotherWhiteKey = new Key(whiteKeys[position], whiteNotes[position],true);
+            Key anotherBlackKey = new Key(blackKeys[position], blackNotes[position],false);
             //showText("Hello " + studentNames[ frames / 60 ], 400, 170);
-            
+
             //This line actually adds the object to the Piano world
             //        OBJECT    Horitontal Position   Vertical position
-            addObject(anotherKey, 54 + position * 63, 140);
-            
+            if (position < 12)
+            {
+                addObject(anotherWhiteKey, 54 + position * 63, 140);
+            }
+
+            if (blackKeys[position] != "" && (position > 12) && (position < 24))
+            {
+                addObject(anotherBlackKey, 85 + position * 63, 75);
+            }
+
             //Only show a message when we are in the bounds of the array
-            showText("Array indext is:" + frames / 60, 400, 250);
+            showText("Array indext is:" + position, 400, 250);
 
         }
 
@@ -67,6 +77,5 @@ public class Piano extends World
         showText("" + frames, 100, 100);
 
     }
-    
-    
+
 }   
