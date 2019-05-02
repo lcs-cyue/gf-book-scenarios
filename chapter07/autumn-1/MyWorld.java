2c5d9fda91c41446a0622e1b9cbc4cfc5be3d03b
Chelsea Yue <cyue@lcs.on.ca>
2019-05-02 11:14:22 -0400

    Example of using a loop to add many objects to the world at once

--- chapter07/autumn-1/MyWorld.java
+++ chapter07/autumn-1/MyWorld.java
@@ -23,5 +23,19 @@ public class MyWorld extends World
     private void setUp()
     {
         addObject(new Block(), 300, 200);
+        
+        //Add 18 leaves object to the world
+        //Start    keep going as long as this true   change by
+        for(int i = 1; i <= 18; i += 1)
+        {
+            Leaf aLeaf = new Leaf();
+            
+            //Get a random location (use getWidth() instead of 600 to avoid a magic constant)
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            
+            //Add the leaf object to the world
+            addObject(aLeaf, x, y);
+        }
     }
 }
